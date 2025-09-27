20cd41108d472ed1bde65f1d1156af3e39320e61
Lars Raeymaekers <lars@rmkrs.be>
2025-09-27 15:36:29 +0200

    fix

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,5 +1,24 @@
 $(document).ready(function() {
     var lastScrollTop = 0;
+    
+    // Detect iOS Safari
+    var isIOSSafari = /iP(ad|hone|od).+Version\/[\d\.]+.*Safari/i.test(navigator.userAgent);
+    
+    // Function to fix footer position on iOS Safari
+    function fixFooterPosition() {
+        if (isIOSSafari) {
+            var footer = $('footer');
+            // Get the actual viewport height
+            var viewportHeight = window.innerHeight;
+            // Force footer to bottom of visual viewport
+            footer.css({
+                'position': 'fixed',
+                'bottom': '0px',
+                'transform': 'translateZ(0)',
+                '-webkit-transform': 'translateZ(0)'
+            });
+        }
+    }
 
     $(window).scroll(function() {
         var currentScrollTop = $(this).scrollTop();
@@ -13,5 +32,19 @@ $(document).ready(function() {
         }
 
         lastScrollTop = currentScrollTop;
+        
+        // Fix footer position on every scroll
+        fixFooterPosition();
     });
+    
+    // Also fix on resize (when URL bar shows/hides)
+    $(window).on('resize orientationchange', function() {
+        fixFooterPosition();
+    });
+    
+    // Initial fix
+    fixFooterPosition();
+    
+    // Fix after a small delay to ensure page is fully loaded
+    setTimeout(fixFooterPosition, 100);
 });
